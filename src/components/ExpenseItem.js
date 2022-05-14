6e2ef2a69e44b9b7fae347178990e96db17ab76c
Frederik Laubisch <kafkaese@gmail.com>
2022-05-14 16:41:15 +0200

    add consts to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 5, 14);
+    const expenseTitle = 'Car insurance';
+    const expenseAmount = 295.22;
 
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className='expense-item__description'>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}€</div>
             </div>
 
         </div>
