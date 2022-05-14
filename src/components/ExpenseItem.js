a0b02fc55ead55565f03f70166315c738e84f3e8
Frederik Laubisch <kafkaese@gmail.com>
2022-05-14 17:51:13 +0200

    split out Date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
     
     return (
         <div className='expense-item'>
-            <div>{props.data.date.toDateString()}</div>
+            <ExpenseDate date={props.data.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.amount}€</div>
