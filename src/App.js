fb1518cebfbc7d8e2f704c1dc3d8633adbd4fc14
Frederik Laubisch <kafkaese@gmail.com>
2022-05-18 18:45:05 +0200

    add NewExpense component group

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses}></Expenses>
    </div>
   );
